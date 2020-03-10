dacd1bae81ed4dd1060fae3b00a2cce33d33fa24
Derek Enos <derek.enos@gmail.com>
2020-03-10 13:10:17 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// this import statement tells webpack to include styles.css in the build
+import css from "./styles.css"
+
+function init() {
+  if (!window.addEventListener) return // Check for IE9+
+
+  let options = INSTALL_OPTIONS
+  let element
+
+  // updateElement runs every time the options are updated.
+  // Most of your code will end up inside this function.
+  function updateElement() {
+    if (!options.enabled) {
+      if (element) {
+        element.remove()
+      }
+      return
+    }
+
+    const location = {selector: "body", method: "prepend"}
+    element = INSTALL.createElement(location, element)
+
+    element.style.backgroundColor = options.backgroundColor
+    element.style.color = options.messageColor
+
+    // Set the app attribute to your app's dash-delimited alias.
+    element.setAttribute("app", "your-app-name")
+    element.innerHTML = `${options.message}<button>X</button>`
+
+    // Remove the element from view on click.
+    element.addEventListener('click', () => {
+      element.classList.remove('visible')
+    })
+
+    // Add the visible vlass to transition the element into view.
+    element.classList.add('visible')
+  }
+
+  // INSTALL_SCOPE is an object that is used to handle option changes without refreshing the page.
+  window.INSTALL_SCOPE = {
+    setOptions(nextOptions) {
+      options = nextOptions
+
+      updateElement()
+    },
+  }
+
+  // This code ensures that the app doesn't run before the page is loaded.
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", updateElement)
+  } else {
+    updateElement()
+  }
+}
+
+init()
